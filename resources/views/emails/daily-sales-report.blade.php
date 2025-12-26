66d2fcf7310185eb80ae72f190e77f1948d3d8a9
Bello Promise <bello.promise@goaptive.com>
2025-12-26 18:17:51 +0100

    Fixed daily report bug

--- resources/views/emails/daily-sales-report.blade.php
+++ resources/views/emails/daily-sales-report.blade.php
@@ -11,17 +11,7 @@
 | Total Items Sold | {{ $totalItemsSold }} |
 | Total Revenue | ${{ number_format($totalRevenue, 2) }} |
 
-@if($orders->count() > 0)
-## Order Details
-
-<x-mail::table>
-| Order ID | Customer | Items | Total |
-|----------|----------|-------|-------|
-@foreach($orders as $order)
-| #{{ $order->id }} | {{ $order->user->name }} | {{ $order->items->sum('quantity') }} | ${{ number_format($order->total_amount, 2) }} |
-@endforeach
-</x-mail::table>
-
+@if($totalOrders > 0)
 ## Products Sold
 
 <x-mail::table>
@@ -38,5 +28,3 @@ No orders were completed today.
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
-
-
